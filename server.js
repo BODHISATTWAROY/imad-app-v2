5b43908866dbed26ec03f5899e539f87e3d5f458
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-12 01:12:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,89 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: 'Article One',
+    heading1:  'Article One',
+    heading2:  'Personal Information:BODHISATTWA ROY',
+    date: 'Feb 11, 2017',
+    content: 
+           `<p>
+              I am Bodhisattwa Roy.
+           </p>
+           <p>
+              I am from Santoshpur,Hooghly.
+              
+              
+           </p>
+           <p>
+             I am an Electrical Engineer.
+           </p>
+           <p>
+             I have passsed my 10th from Kamarpukur Ramkrishna Mission Multipurpose School in 2008 securing 83.5 %.
+           </p>
+           <p>
+              I have passed my 10+2 from Goghat High School in 2010 securing 84.4%(Science).
+           </p>
+           <p>
+             I have completed my B.Tech in Electrical Engineering from Narula Institute of Technology in 2014.
+           </p>
+           <p>
+              I have qualified GATE exam in 2014, 2015 and 2016.
+           </p>
+           <p>
+            I have worked as a Graduate Apprentice Trainee(BOPT) in Garden Reach Shipbuilders & Engineers Limited(GRSE Ltd.) FOJ unit from 29.05.2015 to 28.05.2016.
+           </p>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading1 = data.heading1;
+    var heading2 = data.heading2;
+    var content =data.content;
+    
+  var htmlTemplate = 
+    `<html>
+      <head>
+        <title>
+         ${title}
+        </title>  
+        <meta name="viewport" content="width-device-width, initial-scale-1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+      </head>
+      <body>
+         <div class="container">
+           <div>
+              <a href="/">Home</a>
+           </div>
+           <hr/>
+           <h3>
+              ${heading1}
+           </h3>
+           <h4>
+              ${heading2}
+           </h4>
+           <div>
+              ${date}
+           </div>
+           <div>
+             ${content}
+           </div>
+         </div>
+      </body>      
+    
+    </html>
+  `;
+  return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/Article-One', function(req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'Article-One-Personal-Information.html'));
+     res.send(createTemplatepath(articleOne));
 });
 
 app.get('/Article-Two', function(req, res){
