85b92e74c2667f1e40c3236dd1aa7a52158b35de
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 11:57:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,6 +147,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req, res){
+   counter = counter + 1;
+   res.send(counter.toString());
+    
+});
+
 app.get('/:articleName', function(req, res){
     //articleName == Article-One
     //articles[articleName] == {} content object for Article-One
