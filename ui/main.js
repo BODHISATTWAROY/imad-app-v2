add74463ea58235f985aaec3a0388c2d0332658a
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 23:33:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,9 +11,7 @@ button.onclick = function(){
        if (request.readyState === XMLHttpRequest.DONE){
       // Take Some Action
            if (request.status === 200){
-               var counter = request.responseText;
-               var span = document.getElementById('count');
-               span.innerHTML = counter.toString();
+              
            }
        }
        //Not Done Yet
@@ -28,14 +26,34 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-    //Make A Request To The Server And Send The Name
-    //Capture A List Of Name And Render It As A List
-    var names = ['name1', 'name2', 'name3', 'name4'];
+    
+    //Create a Request Object
+    var request = new XMLHttpRequest();
+    
+      //Capture the response and store it in a variable.
+   request.onreadystatechange = function(){
+       if (request.readyState === XMLHttpRequest.DONE){
+      // Take Some Action
+           if (request.status === 200){
+                //Capture A List Of Name And Render It As A List
+    var names = request.responseText;
+    names + JSON.parse(names);
     var list = '';
     for (var i=0; i< names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
+           }
+       }
+       //Not Done Yet
+   };
     
+    //Make A Request To The Server And Send The Name
+  
+    
+     //Make The Request
+     request.open('GET', 'http://bodhisattwaroy.imad.hasura-app.io/submit-name?name=' +  name, true);
+     request.send(null);
+     
 };
