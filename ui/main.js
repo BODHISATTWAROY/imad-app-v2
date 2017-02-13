80f3ff5e0411490e541e82af8f404ca2fe3f9c0e
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 12:46:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
 //Counter Code
 var button = document.getElementById('counter');
-var counter = 0;
+
 
 button.onclick = function(){
     
-    //Make a request to the counter endpoint.
-    
+    //Ceate a Request Object
+     var request = new XMLHttpRequest();
     //Capture the response and store it in a variable.
-    
-    //Render the variable in the correct span.
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+   request.onreadystatechange = function(){
+       if (request.readyState === XMLHttpRequest.DONE){
+      // Take Some Action
+           if (request.status === 200){
+               var counter = request.responseText;
+               
+           }
+       }
+       //Not Done Yet
+   };
+     //Make The Request
+     request.open('GET', 'http://bodhisattwaroy.imad.hasura-app.io/counter', true);
+     request.send(null);
 };
