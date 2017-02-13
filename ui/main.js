6778db7bb7165bf56f453d0827d4ea9db4226763
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 12:26:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,15 @@
-console.log('Loaded!');
+//Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
 
-// Change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New value';
-
-//Move The Image
-var img = document.getElementById('bodhi1');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function(){
     
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+    //Make a request to the counter endpoint.
+    
+    //Capture the response and store it in a variable.
+    
+    //Render the variable in the correct span.
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
