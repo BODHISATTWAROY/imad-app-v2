9ce667865e65526a45f8ebff03202c779a6e3f41
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 19:12:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,20 @@ button.onclick = function(){
      request.open('GET', 'http://bodhisattwaroy.imad.hasura-app.io/counter', true);
      request.send(null);
 };
+
+//Submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    //Make A Request To The Server And Send The Name
+    //Capture A List Of Name And Render It As A List
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for (var i=0; i,names.length; i++){
+        list += '<li>' + name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
