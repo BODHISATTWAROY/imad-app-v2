d23d7435db00ea27f7f1757612efb54b5e6a772d
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 23:47:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,7 @@ button.onclick = function(){
 };
 
 //Submit Name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
@@ -53,6 +52,8 @@ submit.onclick = function() {
   
     
      //Make The Request
+     var nameInput = document.getElementById('name');
+     var name = nameInput.value;
      request.open('GET', 'http://bodhisattwaroy.imad.hasura-app.io/submit-name?name=' +  name, true);
      request.send(null);
      
