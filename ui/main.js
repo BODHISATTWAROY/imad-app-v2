dfc9711d0ac2c8d2b3d2f0a3bb7c5a5fdfbd4545
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-13 20:10:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function() {
     //Capture A List Of Name And Render It As A List
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for (var i=0; i<names.length; i++){
+    for (var i=0; i< names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
