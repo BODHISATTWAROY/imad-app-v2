f867ba7a024e575fbd09c58605d7406a6778b3c3
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-15 20:27:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,6 +123,12 @@ function createTemplate (data) {
               <a href="/">Home</a>
            </div>
            <hr/>
+           <div>
+               <a href="Article-One" class="container">Article One</a>
+               <a href="Article-Two" class="container">Article Two</a>
+               <a href="Article-Three" class="container">Article Three</a>
+           </div>
+           <hr/>
            <h3>
               ${heading1}
            </h3>
