464c19cc5560b79200c956e4b953a1979f5eff02
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-18 20:16:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,6 +127,7 @@ function createTemplate (data) {
                <a href="Article-One" class="container">Article One</a>
                <a href="Article-Two" class="container">Article Two</a>
                <a href="Article-Three" class="container">Article Three</a>
+               <a href="https://google.com">GOOGLE</a>
            </div>
            <hr/>
            <h3>
