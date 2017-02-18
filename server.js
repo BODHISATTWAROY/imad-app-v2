f0e4c735f1b7860869d7116f024bfcf8e3636255
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-18 20:23:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,8 +127,7 @@ function createTemplate (data) {
                <a href="Article-One" class="container">Article One</a>
                <a href="Article-Two" class="container">Article Two</a>
                <a href="Article-Three" class="container">Article Three</a>
-               <a class="gb_P" data-pid="23" href="https://mail.google.com/mail/" data-ved="0EMIuCAEoAA">Gmail</a>
-           </div>
+               </div>
            <hr/>
            <h3>
               ${heading1}
