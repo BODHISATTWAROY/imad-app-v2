e1a213f04543cdfd047f8ff65e15258d0e81af41
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-02-24 17:28:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,6 +127,7 @@ function createTemplate (data) {
                <a href="Article-One" class="container">Article One</a>
                <a href="Article-Two" class="container">Article Two</a>
                <a href="Article-Three" class="container">Article Three</a>
+               <a href="Sign In" class="container">Log-In Page</a>
                 <a class="dropdown-toggle" id="menu1" data-toggle="dropdown">
                     "Quick Links"  
 						  <span class="caret"></span></a>
