b02f75ea0e4a01bf43424ef5b63426cf0d099b42
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-03-10 21:16:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -260,7 +260,7 @@ app.get('/articles/:articleName', function(req, res){
     //articles[articleName] == {} content object for Article-One
    
      //SELECT * FROM article WHERE title = 'Article-One'
-     pool.query("SELECT * FROM Article WHERE title = '"  + req.params.articleName + "'", function (err,result){
+     pool.query("SELECT * FROM Article WHERE title = '" + req.params.articleName + "'", function (err,result){
         if (err) {
             res.status(500).send(err.toString());
         } else {
