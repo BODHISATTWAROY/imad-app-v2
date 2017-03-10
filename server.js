9f07c3fda1ff010d626110ad1608b20d78d8f83c
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-03-10 21:19:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -267,8 +267,8 @@ app.get('/articles/:ArticleName', function(req, res){
             if (result.rows.length === 0) {
                 res.status(404).send('Article Not Found');
             } else {
-                var articleData = result.rows[0];
-                 res.send(createTemplate(articleData));
+                var ArticleData = result.rows[0];
+                 res.send(createTemplate(ArticleData));
             }
         }
      });
