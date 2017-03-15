6037ce607c1254f74e85fb909802b7f8a7917276
BODHISATTWA ROY <bodhisattwaroy1993@gmail.com>
2017-03-15 10:58:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ app.get('/hash/:input', function(req,res) {
 });
 
 
-app.post('/add.php', function(req, res){
+app.post('/add', function(req, res){
     //username, password
     //{"username": "bodhisattwa", "password": "password"}
     //JSON
